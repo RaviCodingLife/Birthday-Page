94de8aaddb827d613674ec6e4567dada3aa575fc
ravisachin <79781988+ravisachin@users.noreply.github.com>
2022-03-01 10:16:02 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var a=prompt("Enter username");
+ var b=prompt("Enter Password");
+  	if(a== "rituraj" && b=="rituraj")
+    //{
+       //window.location.href="bdy.html";
+	//}
+     //else	
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
